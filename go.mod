a2999fa917a7532e84716e13c387f22eb2b15c12
Artem Yarmoluk <koolgen@gmail.com>
2021-01-19 11:27:52 +0800

    Add unit tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gordonbondon/maxminddb-cidrs
 
 go 1.15
 
-require github.com/oschwald/maxminddb-golang v1.8.0
+require (
+	github.com/oschwald/maxminddb-golang v1.8.0
+	github.com/stretchr/testify v1.6.1
+)
---
+++ pkg/cidrs/cidrs_test.go
@@ -0,0 +1,176 @@
+package cidrs_test
+
+import (
+	"encoding/json"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gordonbondon/maxminddb-cidrs/pkg/cidrs"
+)
+
+// TestNetrowksReader implements cidrs.NetworksReader
+type TestNetrowksReader struct {
+	networks []struct {
+		Country      string
+		Subdivisions []string
+		IP           string
+	}
+	next struct {
+		Country      string
+		Subdivisions []string
+		IP           string
+	}
+}
+
+func (n *TestNetrowksReader) Next() bool {
+	if len(n.networks) > 0 {
+		n.next = n.networks[0]
+		n.networks = n.networks[1:]
+		return true
+	} else {
+		return false
+	}
+}
+
+func (n *TestNetrowksReader) Network(result interface{}) (*net.IPNet, error) {
+	if n.next.Country != "" {
+		network := struct {
+			Country struct {
+				IsoCode string
+			}
+			Subdivisions []struct {
+				IsoCode string
+			}
+		}{
+			Country: struct{ IsoCode string }{IsoCode: n.next.Country},
+		}
+
+		if len(n.next.Subdivisions) > 0 {
+			network.Subdivisions = make([]struct{ IsoCode string }, len(n.next.Subdivisions))
+			for i, s := range n.next.Subdivisions {
+				network.Subdivisions[i] = struct{ IsoCode string }{IsoCode: s}
+			}
+		}
+
+		// I'm so sorry
+		value, _ := json.Marshal(network)
+		json.Unmarshal(value, result)
+	}
+
+	_, ipnet, _ := net.ParseCIDR(n.next.IP)
+
+	return ipnet, nil
+}
+
+func (n *TestNetrowksReader) Err() error {
+	return nil
+}
+
+func TestCIDRs(t *testing.T) {
+	testCases := []struct {
+		name     string
+		reader   *TestNetrowksReader
+		options  cidrs.ListOptions
+		expected []string
+	}{
+		{
+			name: "matches ips for single country",
+			reader: &TestNetrowksReader{
+				networks: []struct {
+					Country      string
+					Subdivisions []string
+					IP           string
+				}{
+					{
+						Country: "GB",
+						IP:      "192.0.2.1/32",
+					},
+					{
+						Country: "NL",
+						IP:      "192.0.2.2/32",
+					},
+					{
+						Country: "GB",
+						IP:      "192.0.2.3/32",
+					},
+				},
+			},
+			options: cidrs.ListOptions{
+				Countries: []cidrs.Country{{ISOCode: "GB"}},
+			},
+			expected: []string{"192.0.2.1/32", "192.0.2.3/32"},
+		},
+		{
+			name: "matches only subdivisions when",
+			reader: &TestNetrowksReader{
+				networks: []struct {
+					Country      string
+					Subdivisions []string
+					IP           string
+				}{
+					{
+						Country:      "GB",
+						Subdivisions: []string{"ENG"},
+						IP:           "192.0.2.1/32",
+					},
+					{
+						Country: "GB",
+						IP:      "192.0.2.2/32",
+					},
+					{
+						Country:      "GB",
+						Subdivisions: []string{"WLS"},
+						IP:           "192.0.2.3/32",
+					},
+				},
+			},
+			options: cidrs.ListOptions{
+				Countries: []cidrs.Country{{ISOCode: "GB", Subdivisions: []string{"ENG"}}},
+			},
+			expected: []string{"192.0.2.1/32"},
+		},
+		{
+			name: "matches for multiple countires",
+			reader: &TestNetrowksReader{
+				networks: []struct {
+					Country      string
+					Subdivisions []string
+					IP           string
+				}{
+					{
+						Country:      "GB",
+						Subdivisions: []string{"ENG"},
+						IP:           "192.0.2.1/32",
+					},
+					{
+						Country: "NL",
+						IP:      "192.0.2.2/32",
+					},
+					{
+						Country:      "GB",
+						Subdivisions: []string{"WLS"},
+						IP:           "192.0.2.3/32",
+					},
+				},
+			},
+			options: cidrs.ListOptions{
+				Countries: []cidrs.Country{
+					{ISOCode: "GB", Subdivisions: []string{"ENG"}},
+					{ISOCode: "NL"},
+				},
+			},
+			expected: []string{"192.0.2.1/32", "192.0.2.2/32"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.options.NetworksReader = tc.reader
+			actual, _ := cidrs.List(&tc.options)
+
+			assert.Equal(t, actual, tc.expected, "results should be equal")
+		})
+	}
+}
